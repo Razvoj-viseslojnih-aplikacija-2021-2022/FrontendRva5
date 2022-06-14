9e9825cb085ce1e0cbdf720d3536b154b7902e74
Vladimir Fabri <vfficial98@gmail.com>
2022-06-14 16:52:48 +0200

    Front deployment spreman

--- FrontEndG5/src/app/constants.ts
+++ FrontEndG5/src/app/constants.ts
@@ -1,4 +1,14 @@
-export const ARTIKL_URI = "http://localhost:8080/artikl";
+//Stare konstante koje smo koristili na vežbama, služe samo za lokalno pokretanje aplikacije
+/* export const ARTIKL_URI = "http://localhost:8080/artikl";
 export const DOBAVLJAC_URI = "http://localhost:8080/dobavljac";
 export const PORUDZBINA_URI = "http://localhost:8080/porudzbina";
-export const STAVKA_PORUDZBINE_URI = "http://localhost:8080/stavkaPorudzbine/forPorudzbina"
+export const STAVKA_PORUDZBINE_URI = "http://localhost:8080/stavkaPorudzbine";
+export const STAVKE_FOR_PORUDZBINA_URI = "http://localhost:8080/stavkaPorudzbine/forPorudzbina"; */
+
+
+//Nove konstante koje gadjaju endPoint-e na deploy-ovanom backend-u
+export const ARTIKL_URI = "https://testingbackendvf.herokuapp.com/artikl";
+export const DOBAVLJAC_URI = "https://testingbackendvf.herokuapp.com/dobavljac";
+export const PORUDZBINA_URI = "https://testingbackendvf.herokuapp.com/porudzbina";
+export const STAVKA_PORUDZBINE_URI = "https://testingbackendvf.herokuapp.com/stavkaPorudzbine";
+export const STAVKE_FOR_PORUDZBINA_URI = "https://testingbackendvf.herokuapp.com/stavkaPorudzbine/forPorudzbina";
